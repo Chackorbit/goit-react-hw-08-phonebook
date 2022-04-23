a76a79cd12d51651dbdae0dcf5a5b5f356bf637a
Dmitriy Vasylchuk <dmvs.9898@gmail.com>
2022-04-23 14:56:15 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,18 @@ import ContactForma from './ContactForma/ContactForma';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import s from './App.module.css';
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className={s.section}>
+      <Routes>
+        <Route exact path="/" />
+        <Route exact path="/register" />
+        <Route exact path="/login" />
+        <Route exact path="/contacts" />
+      </Routes>
+
       <p className={s.title}>Phonebook</p>
       <ContactForma />
 
